ab9a85fc9a12fc5aa4d56ad8bc76a79d417c0111
Steve Fallet <steve.fallet@divtec.ch>
2025-05-14 14:26:52 +0200

    fin exercice rectdyn

--- script.js
+++ script.js
@@ -11,8 +11,13 @@
 // Récupérér les éléments HTML
 const form = document.getElementById('formRect');
 const txtHauteur = document.getElementById('hauteur');
+const txtLargeur = document.getElementById('largeur');
 const divRectangle = document.getElementById('rectangle');
-console.log(form, txtHauteur, divRectangle);
+const selectFond = document.getElementById('fond');
+const chkGras = document.getElementById('gras');
+const chkSouligne = document.getElementById('souligne');
+const txtArea = document.getElementById('txt');
+console.log(txtArea);
 
 // Créer une fonction qui sera appelée lors de l'envoi du formulaire
 function miseAJourFormulaire(event) {
@@ -20,14 +25,40 @@ function miseAJourFormulaire(event) {
     event.preventDefault();
     // Récupérer la valeur de la hauteur et la convertir en entier
     const hauteur = parseInt(txtHauteur.value);
+    const largeur = parseInt(txtLargeur.value);
     // Vérifier que la valeur est un nombre entier positif <= 500
-    if(isNaN(hauteur) || hauteur < 1 || hauteur > 500) {
+    if(
+        isNaN(hauteur) || isNaN(largeur) ||
+        hauteur < 1 || hauteur > 500 ||
+        largeur < 1 || largeur > 500
+    ) {
         alert('La valeur doit être un nombre entre 1 et 500 compris');
         return; // Sortir de la fonction si la valeur n'est pas valide
     }
 
-    // Modifier la hauteur du rectangle
+    // Modifier la hauteur et largeur du rectangle
     divRectangle.style.height = hauteur + 'px';
+    divRectangle.style.width = largeur + 'px';
+
+    // Couleur de fond
+    divRectangle.style.backgroundColor = selectFond.value;
+
+    // Couleur du texte
+    const couleurTexte = document.querySelector('[name="color"]:checked').value;
+    divRectangle.style.color = couleurTexte;
+
+    // Mettre le texte en gras
+    if (chkGras.checked) {
+        divRectangle.style.fontWeight = 'bold';
+    } else {
+        divRectangle.style.fontWeight = 'normal';
+    }
+    // Mettre le texte en souligné avec une ternaire
+    // Si le test chkSouligne.checked est vrai, on met 'underline' sinon 'none'
+    divRectangle.style.textDecoration = chkSouligne.checked ? 'underline' : 'none';
+
+    // Changer le contenu texte du rectangle
+    divRectangle.innerHTML = txtArea.value;
 }
 // Ecouter l'événement submit (envoi) sur le formulaire
 form.addEventListener('submit', miseAJourFormulaire);
